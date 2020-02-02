f33c6bfa1091ee9e3261b4b261cdb7b957fcf11c
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-02-02 12:29:06 -0600

    Update 'src/index.js', add saga middewares, redux store logic, and new '<App />' component (copied over from my previous efforts done in my local machine).

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "regenerator-runtime/runtime";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import PropTypes from 'prop-types';
+import RootReducer from './app/reducers/all';
+import RootSaga from './app/sagas';
+import App from './app/components/App/App';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  RootReducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(RootSaga);
+
+Provider.propTypes = {
+  store: PropTypes.object.isRequired
+};
+
+store.subscribe(() => {
+  // let newState = store.getState();
+  // console.log("New `store` state: ", newState);
+});
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('o-app-root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
