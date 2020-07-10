96e193876e33611956d31032d7dc5f181aa4964c
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-10 16:59:50 -0700

    Add an effect to assign the returned data to local state in order to populate the <ScrollCard />'s.

--- src/app/components/ScrollTrack/ScrollTrack.js
+++ src/app/components/ScrollTrack/ScrollTrack.js
@@ -1,28 +1,69 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux' 
+import { useSelector } from 'react-redux';
 import ScrollCard from '../ScrollCard/ScrollCard';
 import './ScrollTrack.css';
 
 const ScrollTrack = () => {
-  const [transformZ, setTransformZ] = useState();
+  const axiosReducer = useSelector(state => state.axiosReducer);
   const stylesReducer = useSelector(state => state.stylesReducer);
+
+  const [cards, setCards] = useState();
+  const [transformZ, setTransformZ] = useState();
+  
   const transformZValue = stylesReducer.transformZValue;
-  const allCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   const scrollTrackStyles = {
     transform: `translateZ(${transformZ}px)`
   }
 
   useEffect(() => {
-    setTransformZ(transformZValue);
-  }, [transformZValue])
-  
-  return (
-    <div className="o-scroll-track" style={scrollTrackStyles}>
-      {allCards.map((card, i) => {
-        return <ScrollCard key={`${card}-${i}`} cardOrder={card} />
-      })}
-    </div>
-  );
+    let useEffectAborted = false;
+
+    if(!useEffectAborted) {
+      setTransformZ(transformZValue);
+    }
+    
+    return(
+      () => {
+        useEffectAborted = true;
+      }
+    )
+  }, [transformZValue]);
+
+  useEffect(() => {
+    let useEffectAborted = false;
+
+    if(!useEffectAborted && !cards) {
+      let cards = axiosReducer.data.results;
+
+      setCards(cards);
+    }
+    
+    return(
+      () => {
+        useEffectAborted = true;
+      }
+    )
+  }, [axiosReducer, cards]);
+
+  if(cards) {
+    return (
+      <div className="o-scroll-track" style={scrollTrackStyles}>
+        {
+          cards.map((card, i) => {
+            return (
+              <ScrollCard
+                key={`${card.name}-${i}`}
+                cardOrder={i}
+                card={card}
+              />
+            )
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default ScrollTrack;
