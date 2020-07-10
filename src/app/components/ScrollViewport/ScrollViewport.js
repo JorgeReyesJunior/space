9e0c7a0bc5c75375ebf417ee9748ff642076872a
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-10 10:48:51 -0700

    Add new useEffect to specifically handle adding/removing window sroll event listener.

--- src/app/components/ScrollViewport/ScrollViewport.js
+++ src/app/components/ScrollViewport/ScrollViewport.js
@@ -5,7 +5,7 @@ import ScrollContainer from '../ScrollContainer/ScrollContainer';
 import './ScrollViewport.css';
 
 const ScrollViewport = () => {
-  const [height, setHeight] = useState();
+  const [ height, setHeight ] = useState();
   const scrollContainerRef = useRef();
   const dispatch = useDispatch();
   const scrollViewportStyle = {
@@ -14,23 +14,47 @@ const ScrollViewport = () => {
 
 
   useEffect(() => {
+    let useEffectAborted = false;
+    
+    const scrollContainer = scrollContainerRef.current;
+    const scrollContainerPerspective = window.getComputedStyle(scrollContainer);
+    const scrollCards = scrollContainer.getElementsByClassName('c-scroll-card').length;
+    const computedScrollContainerPerspective = parseInt(scrollContainerPerspective.perspective);
+    const viewportHeight = (window.innerHeight + computedScrollContainerPerspective) * scrollCards - (50 * scrollCards);
+
+    if(!useEffectAborted && !height) {
+      setHeight(viewportHeight);
+    }
+
+    return (
+      () => {
+        useEffectAborted = true;
+      }
+    )
+  }, [height]);
+  
+  useEffect(() => {
+    let useEffectAborted = false;
+
     function updateView() {
       dispatch({
         type: `${actions.UPDATE_TRANSFORM_Z_VALUE}`,
         payload: window.pageYOffset
       });
     }
-    
-    const scrollContainer = scrollContainerRef.current;
-    const scrollContainerPerspective = window.getComputedStyle(scrollContainer);
-    const scrollCards = scrollContainer.getElementsByClassName('c-scroll-card').length;
-    const computedScrollContainerPerspective = parseInt(scrollContainerPerspective.perspective);
-    const viewportHeight = (window.innerHeight + computedScrollContainerPerspective) * scrollCards;
 
-    setHeight(viewportHeight);
+    if(!useEffectAborted) {
+      window.addEventListener('scroll', updateView);
+    }
 
-    window.addEventListener('scroll', updateView);
-  }, [dispatch, scrollContainerRef])
+    return (
+      () => {
+        window.removeEventListener('scroll', updateView);
+
+        useEffectAborted = true;
+      }
+    )
+  }, [dispatch]) 
 
   return (
     <div className="o-scroll-viewport" style={scrollViewportStyle}>
