80d9d1e7660112369e88be8fe431f54ff7a2469a
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-13 09:29:42 -0700

    Add new 'fetchLocalData' to saga (adds local data to reducer).

--- src/app/sagas/axiosSaga.js
+++ src/app/sagas/axiosSaga.js
@@ -1,22 +1,29 @@
-import axios from 'axios';
-import { takeLatest, call, put } from 'redux-saga/effects';
+// import axios from 'axios';
+import { takeLatest, /* call, */ put } from 'redux-saga/effects';
 import actions from '../actions';
 
-function* fetchData(action) {
-  const url = action.payload;
+// function* fetchData(action) {
+//   const url = action.payload;
 
-  try {
-    const response = yield call(
-      axios.get,
-      url
-    );
-    yield put(actions.fetchAxiosSuccess(response.data));
+//   try {
+//     const response = yield call(
+//       axios.get,
+//       url
+//     );
+//     yield put(actions.fetchAxiosSuccess(response.data));
     
-  } catch(error) {
-    yield put(actions.fetchAxiosFailure(error));
-  }
+//   } catch(error) {
+//     yield put(actions.fetchAxiosFailure(error));
+//   }
+// }
+
+function* fetchLocalData(action) {
+  const data = action.payload;
+
+  yield put(actions.fetchAxiosSuccess(data.results));
 }
 
 export default function* AxiosSaga() {
-  yield takeLatest('AXIOS_FETCH', fetchData);
+  // yield takeLatest('AXIOS_FETCH', fetchData);
+  yield takeLatest('AXIOS_FETCH', fetchLocalData);
 };
